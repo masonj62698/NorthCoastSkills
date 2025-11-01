81ac04d243957e615c87fb024cd1b0fa85f85e0d
masonj62698 <masoncj@bgsu.edu>
2025-11-01 13:35:40 -0400

    chore: clean up the home page with some bootstrap styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
